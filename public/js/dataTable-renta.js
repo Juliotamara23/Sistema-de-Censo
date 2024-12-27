d578285a4384973ecb14ece0e1b48f3697b4727d
JULIO ANDRES TAMARA HERNANDEZ <julio.tamara23@outlook.com>
2024-12-27 17:07:32 -0500

    Mejoras a la vista renta en formato de tabla

--- public/js/dataTable-renta.js
+++ public/js/dataTable-renta.js
@@ -4,11 +4,40 @@ $(document).ready(function () {
             url: '/js/i18n/es-CO.json'
         },
         buttons: [
-            { extend: 'copy', text: '<i class="bi bi-files text-primary-emphasis"></i>', className: 'btn btn-primary bg-primary-subtle btn-sm me-2 btn-sm me-2 rounded-3', titleAttr: 'Copiar' },
-            { extend: 'excel', text: '<i class="bi bi-file-spreadsheet text-success-emphasis"></i>', className: 'btn btn-success bg-success-subtle btn-sm me-2 btn-sm me-2 rounded-3', titleAttr: 'Excel' },
-            { extend: 'pdf', text: '<i class="bi bi-file-pdf text-danger-emphasis"></i>', className: 'btn btn-danger bg-danger-subtle btn-sm me-2 btn-sm me-2 rounded-3', titleAttr: 'PDF' },
-            { extend: 'print', text: '<i class="bi bi-printer text-secondary-emphasis"></i>', className: 'btn btn-secondary bg-secondary-subtle btn-sm me-2 btn-sm me-2 rounded-3', titleAttr: 'Imprimir' },
-            { extend: 'colvis', text: '<i class="bi bi-eye text-dark-emphasis"></i>', className: 'btn btn-light bg-light-subtle btn-sm me-2 btn-sm me-2 rounded-3', titleAttr: 'Visibilidad' }
+            {
+                text: '<i class="bi bi-clipboard text-primary-emphasis"> Copiar filas</i>',
+                className: 'btn btn-primary bg-primary-subtle me-2 rounded-3',
+                titleAttr: 'Copiar',
+                action: async function () {
+                    var selectedData = table.rows('.selected').data().toArray();
+
+                    // If no rows are selected, copy all data
+                    if (selectedData.length === 0) {
+                        selectedData = table.rows().data().toArray();
+                    }
+
+                    // Generate message for the modal
+                    var message = selectedData.length + ' filas copiadas con éxito\n\n';
+
+                    // Copy selected data to clipboard
+                    var clipboardData = selectedData.map(function (row) {
+                        return row.join('\t');
+                    }).join('\n');
+
+                    try {
+                        await navigator.clipboard.writeText(clipboardData);
+                    } catch (err) {
+                        console.error('Error al copiar al portapapeles: ', err);
+                    }
+
+                    // Show the message modal
+                    $('#updateMessage').text(message);
+                    $('#updateModal').modal('show');
+                }
+            },
+            { extend: 'pdf', text: '<i class="bi bi-file-pdf text-danger-emphasis"> Exportar a PDF</i>', className: 'btn btn-danger bg-danger-subtle me-2 rounded-3', titleAttr: 'PDF' },
+            { extend: 'print', text: '<i class="bi bi-printer text-secondary-emphasis"> Imprimir</i>', className: 'btn btn-secondary bg-secondary-subtle me-2 rounded-3', titleAttr: 'Imprimir' },
+            { extend: 'colvis', text: '<i class="bi bi-eye text-dark-emphasis"> Columnas visibles</i>', className: 'btn btn-light bg-light-subtle me-2 btn-sm me-2 rounded-3', titleAttr: 'Visibilidad' }
         ],
         dom: 'Bfrtip',
         paging: false,
